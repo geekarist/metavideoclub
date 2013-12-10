4b5f716eea80a78895a3e15d0fabee40444894eb
Christophe <christophe.pele@gmail.com>
2013-12-10 13:56:21 +0100

    Use google custom search api to get movie rental url

--- nvodcmp/index.js
+++ nvodcmp/index.js
@@ -11,39 +11,46 @@ function canalPlayUrl() {
     return "http://vod.canalplay.com/films/cinema/batman-begins,297,301,2887.aspx";
 }
 
-function virginMegaUrl() {
+function virginMegaMovieUrl() {
     return "http://www.virginmega.fr/video/film/batman-begins-vost-106730638,normal,page1.htm";
 }
 
-function tf1VodUrl() {
+function tf1VodMovieUrl() {
     return "http://mytf1vod.tf1.fr/collection/363-Super_Heros/media-3599-Batman_Begins.html?PHPSESSID=7bds948p607isk4ftkg388skp6";
 }
 
-function videoFuturUrl() {
-    return "http://www.videofutur.fr/viewTitleDetails.do?titreId=121349";
+function requestMovieUrl(title, siteSearch, callback) {
+    var q = title + ' location';
+    var cx = "005741261737378155414:ptryjmsmugc";
+    var key = "AIzaSyBWMeWVWdEEYvDBvQmM4xCXV-1UcGr4yN8";
+    var searchUrl = "https://www.googleapis.com/customsearch/v1?q=" + escape(q) + "&cx=" + escape(cx) + "&key=" + key + "&siteSearch=" + siteSearch;
+    request(searchUrl, function(error, response, body) {
+        var movieUrl = JSON.parse(body).items[0].link;
+        callback(movieUrl);
+    });
 }
 
 var userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:15.0) Gecko/20120427 Firefox/15.0a1';
 
-var rentalInfoSelector = '.vodRent .butTop:contains("unit")';
-var rentalStore = 'Video Futur';
-request({url:videoFuturUrl(), headers:{'User-Agent':userAgent}}, function(error, response, body) {
-    var $ = cheerio.load(body);
-    var rentalInfo = $(rentalInfoSelector).text();
-    var rentalPrice = parseFloat(rentalInfo.replace(',', '.'));
-    console.log(rentalStore + ': ' + rentalPrice + ' €');
+function logMoviePrice(rentalInfoSelector, rentalStore, rentalUrl) {
+    request({url:rentalUrl, headers:{'User-Agent':userAgent}}, function(error, response, body) {
+        var $ = cheerio.load(body);
+        var rentalInfo = $(rentalInfoSelector).text();
+        var rentalPrice = parseFloat(rentalInfo.replace(',', '.'));
+        console.log(rentalStore + ': ' + rentalPrice + ' €');
+    })
+}
+
+var movieTitle = process.argv.splice(2).join(' ');
+
+requestMovieUrl(movieTitle, "www.videofutur.fr", function(movieUrl) {
+    logMoviePrice('.vodRent .butTop:contains("unit")', 'Video Futur', movieUrl);
 });
 
-request({url:tf1VodUrl(), headers:{'User-Agent':userAgent}}, function(error, response, body) {
-    var $ = cheerio.load(body);
-    var rentalInfo = $('#head-ctn:contains("Louer") span').text();
-    var rentalPrice = parseFloat(rentalInfo.replace(',', '.'));
-    console.log('TF1 VOD: ' + rentalPrice + ' €');
+requestMovieUrl(movieTitle, "mytf1vod.tf1.fr", function(movieUrl) {
+    logMoviePrice('#head-ctn:contains("Louer") span', 'TF1 VOD', movieUrl);
 });
 
-request({url:virginMegaUrl(), headers:{'User-Agent':userAgent}}, function(error, response, body) {
-    var $ = cheerio.load(body);
-    var rentalInfo = $('.cont big').text();
-    var rentalPrice = parseFloat(rentalInfo.replace(',', '.'));
-    console.log('Virgin Mega: ' + rentalPrice + ' €');
+requestMovieUrl(movieTitle, "www.virginmega.fr", function(movieUrl) {
+    logMoviePrice('.cont big', 'Virgin Mega', movieUrl);
 });
