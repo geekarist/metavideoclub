37a5e6cfe4d7abb1caa13f3fea75fa5633ba0b00
Christophe <christophe.pele@gmail.com>
2013-12-11 13:39:38 +0100

    Get movie title from rental store, refactor

--- nvodcmp/index.js
+++ nvodcmp/index.js
@@ -3,54 +3,33 @@
 var cheerio = require('cheerio');
 var request = require('request');
 
-function warnerBrosUrl() {
-    return "http://www.warnerbros.fr/batman-begins-5.html";
-}
+var movieTitle = process.argv.splice(2).join(' ');
 
-function canalPlayUrl() {
-    return "http://vod.canalplay.com/films/cinema/batman-begins,297,301,2887.aspx";
-}
+var USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:15.0) Gecko/20120427 Firefox/15.0a1';
+var STORES = {
+    // canalplay: {name: 'Canal Play', url: 'vod.canalplay.com'},
+    virginmega: {name: 'Virgin Mega', url: "www.virginmega.fr", priceSelector: '.cont big', titleSelector: '.videofile h2'},
+    videofutur: {name: 'Video Futur', url: 'www.videofutur.fr', priceSelector: '.vodRent .butTop:contains("unit")', titleSelector: '.title h2'},
+    tf1vod: {name: 'TF1 VOD', url: "mytf1vod.tf1.fr", priceSelector: '#head-ctn:contains("Louer") span', titleSelector: '#media-title'}
+};
 
-function virginMegaMovieUrl() {
-    return "http://www.virginmega.fr/video/film/batman-begins-vost-106730638,normal,page1.htm";
-}
-
-function tf1VodMovieUrl() {
-    return "http://mytf1vod.tf1.fr/collection/363-Super_Heros/media-3599-Batman_Begins.html?PHPSESSID=7bds948p607isk4ftkg388skp6";
-}
-
-function requestMovieUrl(title, siteSearch, callback) {
-    var q = title + ' location';
+function fetchAndLogMoviePrice(searchTitle, store) {
+    var q = searchTitle + ' location';
     var cx = "005741261737378155414:ptryjmsmugc";
     var key = "AIzaSyBWMeWVWdEEYvDBvQmM4xCXV-1UcGr4yN8";
-    var searchUrl = "https://www.googleapis.com/customsearch/v1?q=" + escape(q) + "&cx=" + escape(cx) + "&key=" + key + "&siteSearch=" + siteSearch;
+    var searchUrl = "https://www.googleapis.com/customsearch/v1?q=" + escape(q) + "&cx=" + escape(cx) + "&key=" + key + "&siteSearch=" + store.url;
     request(searchUrl, function(error, response, body) {
         var movieUrl = JSON.parse(body).items[0].link;
-        callback(movieUrl);
+        request({url:movieUrl, headers:{'User-Agent':USER_AGENT}}, function(error, response, body) {
+            var $ = cheerio.load(body);
+            var rentalInfo = $(store.priceSelector).text() || "-1";
+            var rentalPrice = parseFloat(rentalInfo.replace(',', '.'));
+            var movieTitle = $(store.titleSelector).text().trim() || 'Unknown title';
+            console.log('"' + movieTitle + '": ' + rentalPrice + ' € at ' + store.name);
+        })
     });
 }
 
-var userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:15.0) Gecko/20120427 Firefox/15.0a1';
-
-function logMoviePrice(rentalInfoSelector, rentalStore, rentalUrl) {
-    request({url:rentalUrl, headers:{'User-Agent':userAgent}}, function(error, response, body) {
-        var $ = cheerio.load(body);
-        var rentalInfo = $(rentalInfoSelector).text();
-        var rentalPrice = parseFloat(rentalInfo.replace(',', '.'));
-        console.log(rentalStore + ': ' + rentalPrice + ' €');
-    })
+for (var store in STORES) {
+    fetchAndLogMoviePrice(movieTitle, STORES[store]);
 }
-
-var movieTitle = process.argv.splice(2).join(' ');
-
-requestMovieUrl(movieTitle, "www.videofutur.fr", function(movieUrl) {
-    logMoviePrice('.vodRent .butTop:contains("unit")', 'Video Futur', movieUrl);
-});
-
-requestMovieUrl(movieTitle, "mytf1vod.tf1.fr", function(movieUrl) {
-    logMoviePrice('#head-ctn:contains("Louer") span', 'TF1 VOD', movieUrl);
-});
-
-requestMovieUrl(movieTitle, "www.virginmega.fr", function(movieUrl) {
-    logMoviePrice('.cont big', 'Virgin Mega', movieUrl);
-});
